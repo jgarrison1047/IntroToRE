5e815992065fe32d5ca0e44bc101ed9225781806
kali <kali@kali>
2024-09-01 18:51:56 -0400

    added ex01

---
+++ ex01/ex01.c
@@ -0,0 +1,18 @@
+
+//These are the external libraries we're going to be using
+#include <stdio.h>
+
+
+
+//This is the function signature for main()
+//Main will return an int
+//Main takes two arguments, an int called arc, and an array of strings called argv
+//The first string in argv is always the name of the program we're running
+int main(int argc, char* argv[]) {
+
+    //printf() is a library function that can print to the screen.
+    printf("This is our first C program");
+
+    //Assuming everything went well, we return 0
+    return 0;
+}
